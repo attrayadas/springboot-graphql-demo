d21c09f90306e5674b4338299869c43baa68f162
Attraya Das <attrayaghoshdas@gmail.com>
2024-06-15 12:21:42 +0530

    Add Entity, Repository and configure database in application.properties

---
+++ src/main/java/com/attraya/entity/Product.java
@@ -0,0 +1,26 @@
+package com.attraya.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String category;
+    private Float price;
+    private Integer stock;
+}
